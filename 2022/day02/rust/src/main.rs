0ec7292e5ec7c2972ba6ad003bbb0b77396d5f11
Drew Rawitz <email@drewrawitz.com>
2022-12-04 17:02:06 -0500

    cleanup

--- 2022/day02/rust/src/main.rs
+++ 2022/day02/rust/src/main.rs
@@ -14,34 +14,18 @@ fn get_shape_points(x: &str) -> i32 {
     };
 }
 
-fn get_part_one_hash_map() -> HashMap<&'static str, i32> {
+fn get_hash_map() -> HashMap<&'static str, (i32, &'static str)> {
     let mut combos = HashMap::new();
 
-    combos.insert("A X", DRAW);
-    combos.insert("A Y", WIN);
-    combos.insert("A Z", LOSE);
-    combos.insert("B X", LOSE);
-    combos.insert("B Y", DRAW);
-    combos.insert("B Z", WIN);
-    combos.insert("C X", WIN);
-    combos.insert("C Y", LOSE);
-    combos.insert("C Z", DRAW);
-
-    return combos;
-}
-
-fn get_part_two_hash_map() -> HashMap<&'static str, &'static str> {
-    let mut combos = HashMap::new();
-
-    combos.insert("A X", "Z");
-    combos.insert("A Y", "X");
-    combos.insert("A Z", "Y");
-    combos.insert("B X", "X");
-    combos.insert("B Y", "Y");
-    combos.insert("B Z", "Z");
-    combos.insert("C X", "Y");
-    combos.insert("C Y", "Z");
-    combos.insert("C Z", "X");
+    combos.insert("A X", (DRAW, "Z"));
+    combos.insert("A Y", (WIN, "X"));
+    combos.insert("A Z", (LOSE, "Y"));
+    combos.insert("B X", (LOSE, "X"));
+    combos.insert("B Y", (DRAW, "Y"));
+    combos.insert("B Z", (WIN, "Z"));
+    combos.insert("C X", (WIN, "Y"));
+    combos.insert("C Y", (LOSE, "Z"));
+    combos.insert("C Z", (DRAW, "X"));
 
     return combos;
 }
@@ -52,20 +36,17 @@ fn main() {
 
     let mut part_one_total = 0;
     let mut part_two_total = 0;
-    let part_one_hash = get_part_one_hash_map();
-    let part_two_hash = get_part_two_hash_map();
+
+    let hash = get_hash_map();
 
     for d in data {
-        let part_one_outcome_points = part_one_hash.get(d).unwrap();
-        let part_one_shape_points = get_shape_points(d);
-
-        let part_two_shape_to_play = part_two_hash.get(d).unwrap();
+        let part_two_shape_to_play = hash.get(d).unwrap().1;
         let part_two_shape_points = get_shape_points(part_two_shape_to_play);
         let part_two_combination =
             format!("{} {}", d.chars().next().unwrap(), part_two_shape_to_play);
-        let part_two_outcome_points = part_one_hash.get(&part_two_combination as &str).unwrap();
+        let part_two_outcome_points = hash.get(&part_two_combination as &str).unwrap().0;
 
-        part_one_total += part_one_shape_points + part_one_outcome_points;
+        part_one_total += get_shape_points(d) + hash.get(d).unwrap().0;
         part_two_total += part_two_shape_points + part_two_outcome_points;
     }
 
