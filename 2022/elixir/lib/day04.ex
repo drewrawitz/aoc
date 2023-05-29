3db3ff3cc3cec3bf4e665876f1163f702fc4be5e
Drew Rawitz <email@drewrawitz.com>
2023-05-28 20:46:28 -0400

    day 4 part 1

---
+++ 2022/elixir/lib/day04.ex
@@ -0,0 +1,56 @@
+defmodule Day04.Part1 do
+  def solve(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&find_overlap/1)
+    |> Enum.flat_map(fn x -> x end)
+    |> Enum.filter(fn x -> !is_nil(x) end)
+    |> Enum.sum()
+  end
+
+  def find_overlap(line) do
+    line
+    |> String.split(",")
+    |> Enum.map(fn x ->
+      x
+      |> String.split("-", trim: true)
+      |> Enum.map(&String.to_integer/1)
+    end)
+    |> Enum.map(fn x ->
+      [first, second] = x
+      Enum.to_list(first..second)
+    end)
+    |> Enum.map(&MapSet.new/1)
+    |> Enum.chunk_every(2)
+    |> Enum.map(fn [first, second] ->
+      common = MapSet.intersection(first, second) |> MapSet.to_list()
+
+      cond do
+        length(common) == MapSet.size(first) -> 1
+        length(common) == MapSet.size(second) -> 1
+        true -> nil
+      end
+    end)
+  end
+end
+
+defmodule Day04.Part2 do
+  def solve(input) do
+    "N/A"
+  end
+end
+
+defmodule Mix.Tasks.Day04 do
+  use Mix.Task
+
+  def run(_) do
+    case File.read("input/day04.txt") do
+      {:ok, input} ->
+        IO.puts("Part One: #{Day04.Part1.solve(input)}")
+        IO.puts("Part Two: #{Day04.Part2.solve(input)}")
+
+      {:error, reason} ->
+        IO.puts("Error: #{reason}")
+    end
+  end
+end
---
+++ 2022/elixir/test/day04_test.exs
@@ -0,0 +1,20 @@
+defmodule Day04Test do
+  use ExUnit.Case
+
+  @example_input """
+  2-4,6-8
+  2-3,4-5
+  5-7,7-9
+  2-8,3-7
+  6-6,4-6
+  2-6,4-8
+  """
+
+  test "part one should equal 2" do
+    assert Day04.Part1.solve(@example_input) == 2
+  end
+
+  test "part two should equal N/A" do
+    assert Day04.Part2.solve(@example_input) == "N/A"
+  end
+end
